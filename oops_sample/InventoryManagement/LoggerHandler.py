daf9f00811cea98ee7592797c82b151dc4fc1c96
P-RASHMI <imrisurashmi25@gmail.com>
2021-09-23 22:36:47 +0530

    [Rashmi] Refactor : this is loggerhandler file with code for error and information

--- oops_sample/InventoryManagement/LoggerHandler.py
+++ oops_sample/InventoryManagement/LoggerHandler.py
@@ -4,8 +4,8 @@ logger = logging
 
 # Basic configuration and required format of the file
 
-logger.basicConfig(filename='C:\Users\Charishma\Desktop\rashmi\PYTHONWORK\oops_sample\InventoryManagement\inventorymanagement.log',
+logger.basicConfig(filename='C:\\Users\\Charishma\\Desktop\\rashmi\\PYTHONWORK\\oops_sample\\InventoryManagement\\inventorymanagement.log',
                     level=logging.INFO, format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s')
 
-logger.basicConfig(filename='C:\Users\Charishma\Desktop\rashmi\PYTHONWORK\oops_sample\InventoryManagement\inventorymanagement.log',
+logger.basicConfig(filename='C:\\Users\\Charishma\\Desktop\\rashmi\\PYTHONWORK\\oops_sample\\InventoryManagement\\inventorymanagement.log',
                     level=logging.ERROR, format='%(asctime)s:%(funcName)s:%(levelname)s:%(lineo)d')
